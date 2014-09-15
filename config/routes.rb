7c7262c0bcfe55a6c1d3e76cf63f0805412a8820
Sharell Bryant <sharell.bryant@gmail.com>
2014-09-15 18:25:55 -0400

    Finish layout and routes and add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
-  get 'static_pages/contact'
-  get 'static_pages/about'
+  get 'contact' => 'static_pages#contact'
+  get 'about' => 'static_pages#about'
+  get 'signup' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
