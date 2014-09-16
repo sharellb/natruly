660ef692aef15665adc4bf6acd4a54d557a32db4
Sharell Bryant <sharell.bryant@gmail.com>
2014-09-16 00:01:11 -0400

    Generated profile controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'users/new'
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :users, :only => [:show]
 
   root to: 'static_pages#home'
   get 'contact' => 'static_pages#contact'
   get 'about' => 'static_pages#about'
-  get 'signup' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,9 @@
 ActiveRecord::Schema.define(version: 20140916021928) do
 
   create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "profile_name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
