e0759ce220eb0e2f89cd48aae0af1fe2a649d7af
Sharell Bryant <sharell.bryant@gmail.com>
2014-09-16 20:23:17 -0400

    Deleted extra favorite files

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,11 @@
 class ProfilesController < ApplicationController
 	def show
 		@user = User.find_by_profile_name(params[:id])
+		@videos = current_user.favorite_videos
 		if @user
 			render action: :show
 		else
 			render file: 'public/404', status: 404, formats: [:html]
 		end
-		@videos = current_user.favorite_videos
 	end
 end
