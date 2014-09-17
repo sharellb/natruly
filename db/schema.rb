baac7ab2a16800783cf37082e3f7b26346333e3f
Sharell Bryant <sharell.bryant@gmail.com>
2014-09-17 11:02:03 -0400

    Dropped favorites table

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917014248) do
-
-  create_table "favorites", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "favorited_id"
-    t.string   "favorited_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "favorites", ["favorited_id", "favorited_type"], name: "index_favorites_on_favorited_id_and_favorited_type"
-  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id"
+ActiveRecord::Schema.define(version: 20140917150000) do
 
   create_table "products", force: true do |t|
     t.datetime "created_at"
