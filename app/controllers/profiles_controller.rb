fa42dd403b27a41f71def73c855a7ce4423a73fb
Sharell Bryant <sharell.bryant@gmail.com>
2014-09-25 15:20:12 -0400

    Refactoring

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,8 @@
 class ProfilesController < ApplicationController
 	def show
 		@user = User.find_by_profile_name(params[:id])
-		
+		@videos = @user.get_voted(Video)
+		@salons = @user.get_voted(Salon)
 		if @user
 			render action: :show
 		else
--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,6 @@ class Image < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :salon
 	acts_as_taggable
+
+	validates_presence_of :link
 end
